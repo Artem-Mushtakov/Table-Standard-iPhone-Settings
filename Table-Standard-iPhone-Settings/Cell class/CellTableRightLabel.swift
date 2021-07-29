62917174486b52a2d717644f3bd6096745ca11cc
a.vladimirovich@internet.ru <163Fork>
2021-07-30 01:18:44 +0400

    Add cell table right label
    Добавил ячейку с дополнительным лейблом справа

---
+++ Table-Standard-iPhone-Settings/Cell class/CellTableRightLabel.swift
@@ -0,0 +1,48 @@
+//
+//  CellTableRightLabel.swift
+//  Table-Settings
+//
+//  Created by Артем on 25.07.2021.
+//
+
+import UIKit
+
+class CellTableRightLabel: BasicCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Views
+
+    lazy var rightLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.textAlignment = NSTextAlignment.right
+        
+        return label
+    } ()
+    
+    // MARK:  - Settings
+    
+    private func setupHierarchy(){
+        addSubview(rightLabel)
+    }
+    
+    private func setupLayout() {
+        
+        rightLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            rightLabel.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            rightLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -40)
+        ])
+    }
+    
+}
