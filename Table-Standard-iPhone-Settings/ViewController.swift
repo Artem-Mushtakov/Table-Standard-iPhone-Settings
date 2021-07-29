cf04cc9db2a114409e6e9995626a4af15a8eaf59
a.vladimirovich@internet.ru <163Fork>
2021-07-30 00:54:12 +0400

    Initial Commit

---
+++ Table-Standard-iPhone-Settings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table-Standard-iPhone-Settings
+//
+//  Created by Артем on 30.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
